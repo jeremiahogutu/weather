77cec160372a24310d962ee5aaa44e21b372d332
Jeremiah Ogutu <jogutu@pm.me>
2019-05-30 17:20:22 -0400

    dummy component and gitignore update

---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Weather extends Component {
+    render() {
+        return (
+            <div>
+                weather component
+            </div>
+        );
+    }
+}
+
+export default Weather;
